0bd7a68976523b6aa2afca78c4bd40366667c8a6
Chenxin <tangchenxin6@163.com>
2022-04-14 23:09:52 -0400

    add multiple js and css files

--- server.py
+++ server.py
@@ -19,6 +19,16 @@ def learnpage(id = None):
 
     return render_template('learn.html') 
 
+@app.route('/finishtutorial')
+def finish_tutorial():
+
+    return render_template('finishtutorial.html')
+
+@app.route('/quiz/<id>')
+def quizpage(id = None):
+    score = 0
+    return render_template('quiz.html', ques_index = id, score = score)
+
 # @app.route('/search_result',  methods=['POST'])
 # def search():
 
