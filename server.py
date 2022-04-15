0d1c7b7eed2ac05c94b6b569d8bfdd360b8498da
Chenxin <tangchenxin6@163.com>
2022-04-14 20:38:20 -0400

    basic backend and frontend file

---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import render_template
+from flask import Response, request, jsonify
+import json
+
+app = Flask(__name__)
+
+
+
+# ROUTES
+
+@app.route('/')
+def homepage():
+
+    return render_template('homepage.html')
+ 
+@app.route('/learn/<id>')
+def learnpage(id = None):
+
+    return render_template('learn.html') 
+
+# @app.route('/search_result',  methods=['POST'])
+# def search():
+
+#     return render_template('search_result.html')
+
+
+# @app.route('/view<id>')
+# def view_detail(id = None):
+
+#     return render_template('view_detail.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
+
+
+
